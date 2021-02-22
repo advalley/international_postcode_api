5272e212bdb1a5534c9ade32709fb66ff953f52e
Dennis de Vulder <dennisdevulder@gmail.com>
2021-02-22 15:07:48 +0100

    Create gem

---
+++ lib/international_postcode_api.rb
@@ -0,0 +1,6 @@
+require "international_postcode_api/version"
+
+module InternationalPostcodeApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/international_postcode_api/version.rb
@@ -0,0 +1,3 @@
+module InternationalPostcodeApi
+  VERSION = "0.1.0"
+end
