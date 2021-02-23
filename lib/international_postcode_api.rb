7df58a18b8c676be2901403ea8da212b92fb1809
Dennis de Vulder <dennisdevulder@gmail.com>
2021-02-23 09:33:44 +0100

    Setup configuration structure

--- lib/international_postcode_api.rb
+++ lib/international_postcode_api.rb
@@ -1,6 +1,20 @@
 require "international_postcode_api/version"
 
 module InternationalPostcodeApi
-  class Error < StandardError; end
-  # Your code goes here...
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :api_key, :secret_key, :base_uri
+
+    def initialize
+      @base_uri = 'https://api.postcode.eu/international/v1/'
+    end
+  end
 end
