2cacd1ef87a69b735b5cff6c5acf2a78903e1831
Dennis de Vulder <dennisdevulder@gmail.com>
2021-02-23 15:07:13 +0100

    Add postcode specs

--- spec/international_postcode_api/client_spec.rb
+++ spec/international_postcode_api/client_spec.rb
@@ -35,4 +35,38 @@ RSpec.describe InternationalPostcodeApi::Client do
       expect(response['exception']).to eq('Text is too short, minimum length is 1.')
     end
   end
+
+  describe '#postcode' do
+    it 'Should return the address for the dutch API' do
+      stub_request(:get, "https://api.postcode.eu/nl/v1/addresses/postcode/1000AB/50").
+        to_return(status: 200, body: File.read('spec/fixtures/postcode_nl.txt'))
+
+      response = InternationalPostcodeApi::Client.postcode('1000AB', '50')
+
+      expect(response.dig(:city)).to_not eq(nil)
+      expect(response.dig(:street)).to_not eq(nil)
+    end
+
+    it 'Should return the address for the international API' do
+      stub_request(:get, 'https://api.postcode.eu/international/v1/autocomplete/nld/1000AB%2050').
+        to_return(status: 200, body: File.read('spec/fixtures/postcode_int.txt'))
+      stub_request(:get, "https://api.postcode.eu/international/v1/address/nld4Ixme2mExtLJrEKNiUBpaWlNHYln1ztNGsOVItBZ46IFMiOjg0sZ4YpoyEbSnipOx0bd49up2ixccmi8CkTmvDRGKvKzYSyVAxBDcWyfNMNXZDtdAzxXQpWdZUWBRMor99WWm9PufM").
+         to_return(status: 200, body: File.read('spec/fixtures/details.txt'), headers: {})
+
+      response = InternationalPostcodeApi::Client.postcode('1000AB', '50', 'DE')
+
+      expect(response.dig(:city)).to_not eq(nil)
+      expect(response.dig(:street)).to_not eq(nil)
+    end
+  end
+
+  describe '#supported-countries' do
+    it 'should return a list of supported countries' do
+      stub_request(:get, 'https://api.postcode.eu/international/v1/supported-countries').
+         to_return(status: 200, body: File.read('spec/fixtures/supported_countries.txt'))
+
+      response = InternationalPostcodeApi::Client.supported_countries
+      expect(response.is_a?(Array)).to eq(true)
+    end
+  end
 end
